49a6d78e69fc4818cae98bb20a620b1b3b8bc040
Benji Kay <bdodson@telogical.com>
2014-09-09 16:55:26 -0500

    getting there...

--- listCtrl.js
+++ listCtrl.js
@@ -2,45 +2,60 @@ var toDoApp = angular.module('toDoApp', []);
 
 toDoApp.controller('listCtrl', ['$scope', '$http',
 
-function listCtrl($scope, $http) {
+	function listCtrl($scope, $http) {
 
-	$scope.formData = {};
-	$scope.toDoList = [];
-	$scope.completedList = [];
+		//$scope.formData = {};
+		$scope.completedList = [];
 
-	//retrieve existing to-dos upon landing on page
-	$http.get('/api/items')
-		.success(function(data) {
+		//loop through mongodb array and push the to-do strings into client array (to-do list)
+		function loadList(data) {
+			$scope.toDoList = [];
 			angular.forEach(data, function(value, key){
 				//console.log(key + ' ' ,value);
 				$scope.toDoList.push(value.item);
 			});
-		})
-		.error(function(data) {
+		}
+
+		function logError(data) {
 			console.log('Error: ' + data);
-	});
+		}
 
-	$scope.itemAdd = function() {
-		$http.post('api/items', $scope.formData)
+		//retrieve existing to-dos upon landing on page
+		$http.get('/api/items')
 			.success(function(data) {
-				$scope.formData = {};
-				$scope.items = data;
-			});
-	};
-
-	$scope.itemRemove = function(id) {
-
-		$scope.completedList.push(id); //display completed items for this session
-
-		$http.delete('/api/items/' + id)
-			.success(function(data) {
-				$scope.items = data;
+				loadList(data);
 			})
 			.error(function(data) {
-				console.log('Error: ' + data);
+				logError(data);
 			});
-	};
 
-}
+		$scope.itemAdd = function() {
+			var item = {};
+			item.text = $scope.toDoItem;
+			console.log(item.text);
+
+			$http.post('/api/items', item)
+				.success(function(data) {
+					loadList(data);
+				})
+				.error(function(data) {
+					logError(data);
+				});
+		};
+
+		$scope.itemRemove = function(id) {
+
+			$scope.completedList.push(id); //display completed items for this session
+
+			$http.delete('/api/items/' + id)
+				.success(function(data) {
+					loadList(data);
+				})
+				.error(function(data) {
+					logError(data);
+				});
+		};
+
+	}
 
 ]);
