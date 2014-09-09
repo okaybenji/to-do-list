4e3db04b5dfeb0fd3b348b0029744f7cdb67052c
Benji Kay <bdodson@telogical.com>
2014-09-09 15:22:10 -0500

    getting there...

--- listCtrl.js
+++ listCtrl.js
@@ -1,5 +1,11 @@
-function listCtrl($scope) {
+var toDoApp = angular.module('toDoApp', []);
 
+toDoApp.controller('listCtrl', ['$scope', '$http',
+
+function listCtrl($scope, $http) {
+
+	// USE ARRAY:
+	/*
 	//$scope.toDoList = ["Learn AngularJS","Learn Node.js","Learn Git"];
 	//$scope.completedList = ["Go to work","Shave a cat","Pee pants (not mine)"];
 
@@ -15,4 +21,43 @@ function listCtrl($scope) {
 		$scope.completedList.push(item);
 		$scope.toDoList.splice($scope.toDoList.indexOf(item), 1);
 	}
+	*/
+
+	// USE MONGODB:
+
+	$scope.formData = {};
+	$scope.completedList = [];
+
+	//retrieve existing to-dos upon landing on page
+	$http.get('/api/items')
+		.success(function(data) {
+			$scope.toDoList = data;
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+	});
+
+	$scope.itemAdd = function() {
+		$http.post('api/items', $scope.formData)
+			.success(function(data) {
+				$scope.formData = {};
+				$scope.items = data;
+			});
+	};
+
+	$scope.itemRemove = function(id) {
+
+		$scope.completedList.push(id); //display completed items for this session
+
+		$http.delete('/api/items/' + id)
+			.success(function(data) {
+				$scope.items = data;
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};
+
 }
+
+]);
--- toDo.js
+++ toDo.js
@@ -1,7 +1,69 @@
 var express = require('express');
 var app = express();
 
+var mongoose = require('mongoose');
+var dbItem = mongoose.model('item', {
+	text : String
+});
+
+mongoose.connect('mongodb://localhost:27017/toDoList')
+
 app.use(express.static(__dirname));
+
+function getItems(req, res) {
+	dbItem.find(function(err, items) {
+		if (err) {
+			res.send(err);
+		}
+		res.json(items); //return items as JSON
+	});
+};
+
+app.get('/api/items', function(req, res) {
+	getItems(req, res);
+});
+
+/*
+//create item
+app.post('/api/items', function(req, res) {
+	dbItem.create({
+		text : req.body.text,
+		done : false
+	}, function(err, item) {
+		if (err) {
+			res.send(err);
+		}
+		getItems(req, res);
+	});
+});
+
+//delete item
+app.delete('/api/items/:item_id', function(req, res) {
+	dbItem.remove({
+		_id : req.params.item_id
+	}, function(err, item) {
+		if (err) {
+			res.send(err)
+		}
+		getItems(req, res);
+	});
+});
+*/
+
+//create item
+app.post('/api/items', function(req, res) {
+	dbItem.create({
+		text : req.body.text,
+		done : false
+	}, getItems(req, res));
+});
+
+//delete item
+app.delete('/api/items/:item_id', function(req, res) {
+	dbItem.remove({
+		_id : req.params.item_id
+	}, getItems(req, res));
+});
 
 app.get('/', function(req, res){
 	res.sendfile("./index.html");
